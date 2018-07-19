9fdaf67dc0766fa17bd687deccf79647783292f5
kamera <asahi20000124@icloud.com>
2018-07-19 22:51:20 +0900

    Update
    PlayerのBullet発射音が消えていたので戻した（EnemybranchでPlayerの修正をするな）

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -148,6 +148,8 @@ public class Player : MonoBehaviour {
 
             }
         }*/
+        soundEffect.clip = shotSoundEffect;
+        soundEffect.Play();
         Instantiate(Bullet,transform.position,transform.rotation);
     }
 
